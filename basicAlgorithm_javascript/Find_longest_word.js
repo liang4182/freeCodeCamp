929f1ea47f3a7aa577046fe3a4220cdfdec90b13
Liang <l68.zhang@hdr.qut.edu.au>
2017-06-14 20:28:34 +1000

    Create Find_longest_word.js

---
+++ basicAlgorithm_javascript/Find_longest_word.js
@@ -0,0 +1,13 @@
+
+function findLongestWord(str) {
+  var myArray = str.split(" ");
+  var wordLength = 0;
+  for(var i=0; i<myArray.length; i++){
+    if (wordLength < myArray[i].length){
+      wordLength = myArray[i].length;
+    }
+  }
+  return wordLength;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
