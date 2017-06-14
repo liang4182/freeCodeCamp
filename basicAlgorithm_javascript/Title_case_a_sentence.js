f99aa0932c7f2b0e26e90f872ebdbfbb22b3af96
Liang <l68.zhang@hdr.qut.edu.au>
2017-06-14 20:30:28 +1000

    Create Title_case_a_sentence.js
    Return the provided string with the first letter of each word capitalised. Make sure the rest of the word is in lower case.

---
+++ basicAlgorithm_javascript/Title_case_a_sentence.js
@@ -0,0 +1,15 @@
+
+function titleCase(str) {
+  var myArray = str.split(" ");
+  var myArray2 = [];
+  for (var i = 0; i < myArray.length; i++){
+    myArray[i] = myArray[i].toLowerCase();
+    myArray2 = myArray[i].split("");
+    myArray2[0] = myArray2[0].toUpperCase();
+    myArray[i] = myArray2.join("");
+  }
+  str = myArray.join(" ");
+  return str;
+}
+
+titleCase("I'm a little tea pot");
